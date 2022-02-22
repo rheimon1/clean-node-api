b29385b8c64641a1f51b4087bdbd26fe71dbc539
Wallace Ramon <wallace.soares@pontaltech.com.br>
2022-02-22 11:38:20 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         body: new Error('Missing param: name'),
