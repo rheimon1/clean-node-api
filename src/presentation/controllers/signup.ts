7d297885f880da5a4c189d7d02aa9ba9031bf1cb
Wallace Ramon <wallace.soares@pontaltech.com.br>
2022-02-22 12:00:50 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,14 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        body: new MissingParamError('name'),
-        statusCode: 400
-      }
+      return badRequest(new MissingParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        body: new MissingParamError('email'),
-        statusCode: 400
-      }
+      return badRequest(new MissingParamError('email'))
     }
     return {
       body: 'Ok',
