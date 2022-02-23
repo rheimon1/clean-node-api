d12b66265e837eb3c706a71f003d823f33e50372
Wallace <wallacerheimon@gmail.com>
2022-02-23 20:31:05 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/*-protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jest-environment-node',
   transform: {
